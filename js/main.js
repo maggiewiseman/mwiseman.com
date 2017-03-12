c7ecd0a1ea97f3cad63796eb6382388362b15041
Maggie <maggie.wiseman@gmail.com>
2017-03-12 17:13:16 -0400

    styledbuttons

--- js/main.js
+++ js/main.js
@@ -5,14 +5,12 @@
 //});
 
 
-$(document).on('mouseenter', '.grid-item-btn, .grid-item-project-btn, .email-btn', function(e) {
-	var event = e;
-	console.log(event);
-	$(e.target).toggleClass('btn-hover');
-	
-	$(e.target.childNodes[1].id).toggleClass('btn-hover-a');
-})
-
-$(document).on('mouseleave', '.grid-item-btn, .grid-item-project-btn, .email-btn', function(e) {
-	$(e.target).toggleClass('btn-hover');
-})
+//$(document).on('mouseenter', '.grid-item-btn, .grid-item-project-btn, .email-btn', function(e) {
+//	var event = e;
+//	console.log(event);
+//	$(e.target).toggleClass('btn-hover');
+//})
+//
+//$(document).on('mouseleave', '.grid-item-btn, .grid-item-project-btn, .email-btn', function(e) {
+//	$(e.target).toggleClass('btn-hover');
+//})
