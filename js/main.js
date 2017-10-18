0e24def90172c1547365f0ef8dcccccead1c32b2
maggiewiseman <maggie.wiseman@gmail.com>
2017-10-18 17:21:19 -0400

    added some logic for button clicking, need to style open class etc.

--- js/main.js
+++ js/main.js
@@ -14,3 +14,13 @@
 //$(document).on('mouseleave', '.grid-item-btn, .grid-item-project-btn, .email-btn', function(e) {
 //	$(e.target).toggleClass('btn-hover');
 //})
+
+(function() {
+    var showMenuBtn = document.getElementById('menu-icon');
+
+    showMenuBtn.addEventListener('click', function(e) {
+        console.log("clicked!", e.target);
+        document.getElementsByTagName('aside')[0].classList.add('open');
+    });
+
+}());
