70e84b78e4987eb571f42187ea2f946534caa60f
maggiewiseman <maggie.wiseman@gmail.com>
2017-10-19 18:31:06 -0400

    clicking menu item removes menu

--- js/main.js
+++ js/main.js
@@ -17,10 +17,22 @@
 
 (function() {
     var showMenuBtn = document.getElementById('menu-icon');
+    var hideMenuBtn = document.getElementById('hide-menu-btn');
+    var aside = document.getElementsByTagName('aside')[0];
+    var list = document.getElementById('#nav-list');
+    console.log(list);
 
     showMenuBtn.addEventListener('click', function(e) {
         console.log("clicked!", e.target);
-        document.getElementsByTagName('aside')[0].classList.add('open');
+        aside.classList.add('open');
     });
 
+    hideMenuBtn.addEventListener('click', function(e) {
+        console.log("clicked close");
+        aside.classList.remove('open');
+    });
+
+    list.addEventListener('click', function(e) {
+        aside.classList.remove('open');
+    });
 }());
